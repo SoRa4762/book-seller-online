0c284a28e4784582092df5ba727c4630723c89e2
SoRa <sonamraisx007@gmail.com>
2023-07-13 23:28:50 +0545

    font awesome added

--- elements/searchBar/page.tsx
+++ elements/searchBar/page.tsx
@@ -1,15 +1,24 @@
 "use client";
 
+import { useState } from "react";
 import { Input } from "../../components/ui/input";
 
 type Props = {};
 
 const SearchBar = (props: Props) => {
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+  };
+  console.log(searchQuery);
+
   return (
     <div>
       <Input
         className=" h-8 lg:w-[50vw] rounded-xl"
         placeholder="🔍 Search..."
+        value={searchQuery}
+        onChange={(e) => onChange(e)}
       />
     </div>
   );
