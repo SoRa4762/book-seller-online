a511a67bf6136ddffb2ad8eff231e4244c9755a6
SoRa <sonamraisx007@gmail.com>
2023-07-16 15:52:46 +0545

    events complete

--- elements/eventsCard/page.tsx
+++ elements/eventsCard/page.tsx
@@ -13,12 +13,12 @@ type Props = {
 const Card = ({ id, title, desc, price, img, alt }: Props) => {
   return (
     <>
-      <div className="h-full w-full cursor-pointer">
-        <div className="flex flex-col">
+      <div className="cursor-pointer">
+        <div className="flex flex-col h-48">
           <Image
-            className="rounded-sm shadow-md shadow-slate-400"
-            height={50}
-            width={500}
+            className="rounded-sm shadow-md h-full w-full object-cover shadow-slate-400"
+            height={1000}
+            width={2000}
             // fill
             alt={alt}
             src={img}
@@ -28,7 +28,7 @@ const Card = ({ id, title, desc, price, img, alt }: Props) => {
             <p className="text-green-500">{price}</p>
           </div>
         </div>
-        <p className="text-xs lg:text-sm text-slate-500 line-clamp-6 my-2">
+        <p className="text-xs lg:text-sm text-slate-500 line-clamp-6 mt-8 mb-4">
           {desc}
         </p>
       </div>
