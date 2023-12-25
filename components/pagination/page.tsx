9d230cc017d007377eeea29b06bf5e5a2192ee50
SoRa <sonamraisx007@gmail.com>
2023-12-25 15:42:59 +0545

    pagination started

---
+++ components/pagination/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { pagination } from '@/app/helper/types'
+
+function Pagination({totalPages}: pagination) {
+  return (
+    <div>Pagination</div>
+  )
+}
+
+export default Pagination
